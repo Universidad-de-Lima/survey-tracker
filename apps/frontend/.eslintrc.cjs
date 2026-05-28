683075ba23f92e4b7f6eb859ebee1437d6b91221
Juan Loayza <jloayzac@ulima.edu.pe>
2026-05-28 16:12:42 -0500

    fix: add pnpm-lock.yaml and inline eslint config for network drive

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -1,12 +1,19 @@
 module.exports = {
   root: true,
-  extends: ['@survey-tracker/eslint-config'],
+  env: { es2022: true, node: true },
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
+  plugins: ['@typescript-eslint', 'import'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:import/typescript'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'error',
+    'import/order': ['error', { groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'], 'newlines-between': 'always', alphabetize: { order: 'asc', caseInsensitive: true } }],
+    'import/no-duplicates': 'error',
+    'no-console': ['warn', { allow: ['warn', 'error', 'info', 'log'] }],
+    'prefer-const': 'error', 'no-var': 'error', eqeqeq: ['error', 'always'],
+  },
   overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      parserOptions: {
-        project: './tsconfig.json',
-      },
-    },
+    { files: ['*.js'], rules: { '@typescript-eslint/no-var-requires': 'off' } },
   ],
 };
